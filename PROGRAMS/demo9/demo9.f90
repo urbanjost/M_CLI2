a177b0077819571815fa6a8da6980bcb45443858
init current directory[i] <urbanjost@comcast.net>
2020-09-19 20:04:08 -0400

    deleted scratch file

--- PROGRAMS/demo9/demo9.f90
+++ PROGRAMS/demo9/demo9.f90
@@ -1,4 +1,4 @@
-program demo3
+program demo9
 !! QUICK PROTOTYPE: JUST THE BARE ESSENTIALS
 use M_CLI2,  only : set_args, get_args
 use M_CLI2,  only : sget, rget, iget, lget
@@ -7,4 +7,4 @@ implicit none
    call set_args('-x 1 -y 10 --size 12.34567  -l F --title "my title"')
    !write(*,'(*("[",g0,"]":,1x))')iget('x'),iget('y'),rget('size'),lget('l'),sget('title')
    write(*,'(*("[",g0,"]":,1x))')igets('x'),igets('y'),rgets('size'),lgets('l'),sgets('title')
-end program demo3
+end program demo9
