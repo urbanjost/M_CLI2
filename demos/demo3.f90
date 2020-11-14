278955d4c29237b01c29b98ae2a7c0355eba7632
init current directory[i] <urbanjost@comcast.net>
2020-11-14 15:31:15 -0500

    make demo3 shorter

--- demos/demo3.f90
+++ demos/demo3.f90
@@ -1,16 +1,14 @@
 program demo3
-!! QUICK PROTOTYPE: JUST THE BARE ESSENTIALS
+!*! JUST THE BARE ESSENTIALS
 use M_CLI2,  only : set_args, get_args
 implicit none
 integer                      :: x, y
 logical                      :: l
 real                         :: size
 character(len=:),allocatable :: title
-   call set_args('-x 1 -y 10 --size 12.34567  -l F --title "my title"')
-   call get_args('x',x)
-   call get_args('y',y)
-   call get_args('l',l)
-   call get_args('size',size)
+   call set_args('-x 1 -y 10 --size:s 12.34567  -l F --title:t "my title"')
+   call get_args('x',x, 'y',y,'l',l, 'size',size) ! all the non-allocatables
    call get_args('title',title)
+   ! all variables set and of the right type
    write(*,'(*("[",g0,"]":,1x))')x,y,size,l,title
 end program demo3
